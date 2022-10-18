0a03f69c62bd580f5a56ad27bfa67b9bfc7bd9a6
ItzAnsh <anshbhasin3723@gmail.com>
2022-10-18 15:33:03 +0530

    scroll snapping effect

--- src/sections/Track/Track.js
+++ src/sections/Track/Track.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import Cards from './Cards'
-import Text from './Text'
-import "./style.css"
-import Cards1 from './Cards1'
+import React from "react";
+import Cards from "./Cards";
+import Text from "./Text";
+import "./style.css";
+import Cards1 from "./Cards1";
 
 const Tracks = () => {
-    
-    return (
-        <div id='tracks' className='tracksContainer items-center flex max-w-[100vw] text-white bg-slate-900'>
-            <Text/>
-            {/* <Cards/> */}
-            <Cards1 />
-        </div>
-    )
-}
+	return (
+		<div
+			id="tracks"
+			className="tracksContainer items-center flex max-w-[100vw] text-white bg-slate-900 snap-start"
+		>
+			<Text />
+			{/* <Cards/> */}
+			<Cards1 />
+		</div>
+	);
+};
 
-export default Tracks
+export default Tracks;
