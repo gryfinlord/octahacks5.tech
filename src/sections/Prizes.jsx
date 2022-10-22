91c61b8fcffd856a74b1277085f8676c2bdc5981
ItzAnsh <anshbhasin3723@gmail.com>
2022-10-22 17:21:55 +0530

    changes

---
+++ src/sections/Prizes.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+function Prizes() {
+	return (
+		<div
+			id="prizes"
+			className="min-h-screen bg-slate-900 bg-[url(https://github.com/ikaushiksharma/WhatsApp_Ui/blob/master/backVideo.gif?raw=true)] bg-no-repeat bg-[length:100%_100%] grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 w-full xl:h-[84%] lg:h-[84%] h-[88%] p-2 snap-start h-screen"
+		>
+			<div className="flex flex-col justify-center items-center backdrop-blur-lg bg-black/30">
+				<div className="p-2 flex flex-col justify-center items-center lg:ml-4">
+					<div className="md:w-full lg:w-[88%] h-full flex flex-col justify-center items-center md:items-start text-white bg-transparent  cursor-pointer space-y-4 ">
+						<h2 className="text-[1.2rem] lg:text-[1.3rem] xl:text-[1.6rem] font-medium">
+							'TECH IT EASY'
+						</h2>
+						<h1 className="text-4xl lg:text-5xl xl:text-6xl font-normal text-transparent bg-gradient-to-r from-[#6c6aff] via-[#fbc7d4] to-[#ff658b] inline-block bg-clip-text">
+							Prizes
+						</h1>
+						<p className="text-[1rem] lg:text-[1.2rem] xl:text-[1.4rem] font-light md:text-left text-center">
+							Just to incentivise your hard work and effort over those 36 hours
+							we have prizes, cash rewards and more lined up for the top
+							performers by our generous Sponsors. Swags and goodies are for
+							everyone but for the ones with the most creative and innovative
+							projects we have more lined up!!
+						</p>
+					</div>
+				</div>
+			</div>
+			<div className="flex flex-col justify-center items-center backdrop-blur-lg bg-black/30">
+				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 w-full h-full  items-center px-2 py-5 gap-2">
+					<div className="w-full h-full flex flex-col justify-center items-center text-white bg-gray-800 p-2 cursor-pointer rounded-[4px]">
+						<h2 className="text-xl font-semibold p-[2%] text-center">Winner</h2>
+						<h1 className="text-[2rem] font-black text-center">₹ 15,000*</h1>
+						<p className="text-[.5rem] lg:text-[.8rem] text-center">
+							+Goodies worth upto
+						</p>
+						<p>₹ 1 Lakh</p>
+					</div>
+					<div className="w-full h-full flex flex-col justify-center items-center text-white bg-gray-800 p-2 cursor-pointer rounded-[4px]">
+						<h2 className="text-xl font-semibold p-[2%] text-center">
+							Runner Up
+						</h2>
+						<h1 className="text-[2rem] font-black text-center">₹ 10,000*</h1>
+						<p className="text-[.5rem] lg:text-[.8rem] text-center">
+							+Goodies worth upto
+						</p>
+						<p>₹ 75 Thousand</p>
+					</div>
+					<div className="w-full h-full flex flex-col justify-center items-center text-white bg-gray-800 p-2 cursor-pointer rounded-[4px]">
+						<h2 className="text-xl font-semibold p-[2%] text-center">Third</h2>
+						<h1 className="text-[2rem] font-black text-center">₹ 5,000*</h1>
+						<p className="text-[.5rem] lg:text-[.8rem] text-center">
+							+Goodies worth upto
+						</p>
+						<p>₹ 30 Thousand</p>
+					</div>
+					<div className="w-full h-full flex flex-col justify-center items-center text-white bg-gray-800 p-2 cursor-pointer rounded-[4px] space-y-2">
+						<h2 className="h-auto w-[28%] sm:w-1/2 md:w-[75%] p-[2%] ">
+							<img src="	https://octahacks4.tech/assets/Sponsors/present/tezos.svg" />
+						</h2>
+						<p className="text-[.7rem]  md:text-[.6rem] lg:text-[.8rem] text-center">
+							₹ 20,000* for best Dapp built on Tezos.
+						</p>
+						<p className="text-[.7rem]  md:text-[.6rem]  lg:text-[.8rem] text-center">
+							+Continuity grant opportunity up to 5,000 USD for an outstanding
+							project.
+						</p>
+					</div>
+					<div className="w-full h-full flex flex-col justify-center items-center text-white bg-gray-800 p-2 cursor-pointer rounded-[4px] space-y-2">
+						<h2 className="h-auto w-[28%] sm:w-1/2 md:w-[75%] p-[2%] ">
+							<img src="https://octahacks4.tech/assets/Sponsors/present/polygon.svg" />
+						</h2>
+						<p className="text-[.7rem]  md:text-[.6rem]  lg:text-[.8rem] text-center">
+							₹ 20,000* for best Dapp built on Tezos.
+						</p>
+						<p className="text-[.7rem]  md:text-[.6rem]  lg:text-[.8rem] text-center">
+							+Continuity grant opportunity up to 5,000 USD for an outstanding
+							project.
+						</p>
+					</div>
+					<div className="w-full h-full flex flex-col justify-center items-center text-white bg-gray-800 p-2 cursor-pointer rounded-[4px] space-y-2">
+						<h2 className="h-auto w-[28%] sm:w-1/2 md:w-[75%] p-[2%] ">
+							<img src="	https://octahacks4.tech/assets/Sponsors/present/celo.png" />
+						</h2>
+						<p className="text-[.7rem]  md:text-[.6rem] lg:text-[.8rem] text-center">
+							₹ 20,000* for best Dapp built on Tezos.
+						</p>
+						<p className="text-[.7rem] md:text-[.6rem] lg:text-[.8rem] text-center">
+							+Continuity grant opportunity up to 5,000 USD for an outstanding
+							project.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+export default Prizes;
