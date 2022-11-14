c779ddcb12eeff8a9c42b947adf0fad0e2c88c89
YAKSHIT-22 <yakshitgarg25@gmail.com>
2022-11-15 00:35:44 +0530

    minor changes2.0

--- src/sections/Landing/Landing.jsx
+++ src/sections/Landing/Landing.jsx
@@ -20,26 +20,20 @@ const Landing = () => {
 					</p>
 				</div>
 				<div className="w-2/12 h-[1px] bg-white rounded-lg my-8 mx-4 border-white border-2"></div>
-                <div className="flex items-center flex-col lg:flex-row justify-center gap-7">
+                <div className="flex items-center flex-col lg:flex-row justify-center gap-7 w-[75%] md:w-1/2 lg:w-[75%] xl:w-1/2 p-4 m-1">
 					
-				<button className="p-2  w-[22.5rem] flex items-center justify-center gap-4 sm:text-sm md:text-md lg:text-lg  font-semibold border-2 border-blue-700">
+				<button className="p-2  w-full flex items-center justify-center gap-4 text-sm md:text-md lg:text-lg  font-semibold border-2 border-blue-700">
 					Apply With Devfolio
-					<span className="flex  items-center relative text-2xl transition-all duration-150 ">
-						<span className="z-10"> &rarr;</span>
-						<div className="w-6 h-6 absolute border border-blue-400 left-3 group-hover:bg-blue-500  transition-all ease-in duration-150"></div>
-					</span>
+					
 				</button>
-				<button className="p-2 w-[20rem] flex items-center  justify-center gap-4 sm:text-sm md:text-md lg:text-lg font-semibold border-violet-700 border-2">
+				<button className="p-2 w-full flex items-center  justify-center gap-4 text-sm md:text-md lg:text-lg font-semibold border-violet-700 border-2">
 					Join Discord
-					<span className="flex  items-center relative text-2xl transition-all duration-150 ">
-						<span className="z-10"> &rarr;</span>
-						<div className="w-6 h-6 absolute border border-blue-400 left-3 group-hover:bg-blue-500  transition-all ease-in duration-150"></div>
-					</span>
+					
 				</button>
 				
 				</div>
 			</div>
-			<div className="flex-col justify-center item-center flex absolute right-6 xs:right-6 sm:right-8 md:right-10 lg:right-12 xl:right-14 gap-[1px] bottom-6">
+			<div className="flex-col justify-center item-center flex absolute right-6 xs:right-6 sm:right-8 md:right-10 lg:right-12 xl:right-14 gap-[1px] bottom-4">
 				<img
 					alt="socials"
 					className="p-1 h-8 w-8"
