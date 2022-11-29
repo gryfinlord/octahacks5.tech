71591d0eb62d3e07b8bf98a062aa1defccdbb799
ItzAnsh <anshbhasin3723@gmail.com>
2022-11-29 23:20:18 +0530

    change

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const About = () => {
 	return (
-		<div className="flex flex-col md:justify-between items-center lg:flex-row  w-full p-4 xs:p-10 sm:p-16  lg:p-12 gap-2 lg:h-[80vh] bg-slate-900" id="about">
-			<div className="text-3xl lg:text-5xl xl:text-6xl font-bold text-transparent md:text-4xl lg:w-1/2 bg-clip-text  bg-gradient-to-r from-[#6c6aff] via-[#fbc7d4] to-[#ff658b] text-center lg:text-start mt-6 lg:mt-0">
-				About Us
+		<div
+			className="flex flex-col md:justify-between items-center lg:flex-row  w-full p-4 xs:p-10 sm:p-16  lg:p-12 gap-2 lg:h-[80vh] bg-slate-900"
+			id="about"
+		>
+			<div className="text-3xl lg:text-5xl xl:text-6xl font-bold md:text-4xl lg:w-1/2  text-center lg:text-start mt-6 lg:mt-0">
+				<span className="bg-gradient-to-r from-[#6c6aff] via-[#fbc7d4] to-[#ff658b] bg-clip-text text-transparent">
+					About Us
+				</span>
 			</div>
 			<div className="text-[1.2rem] lg:text-[1.3rem] xl:text-[1.5rem]  p-4 text-center lg:text-start lg:w-3/4 font-medium">
 				<p>
