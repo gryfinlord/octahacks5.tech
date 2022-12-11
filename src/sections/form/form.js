bc2f279ea49e793cf524583f12b5dd9654215199
ItzAnsh <anshbhasin3723@gmail.com>
2022-12-11 17:37:03 +0530

    changes

--- src/sections/form/form.js
+++ src/sections/form/form.js
@@ -1,11 +1,34 @@
 import React from "react";
-
+import { useState } from "react";
+import axios from "axios";
 export default function Form(props) {
+	const [fName, setFName] = useState("");
+	const [lName, setLName] = useState("");
+	const [phone, setPhone] = useState("");
+	const [gender, setGender] = useState("");
+	const [year, setYear] = useState("");
+	const [school, setSchool] = useState("");
+	const [bio, setBio] = useState("");
+	const handleClick = () => {
+		axios
+			.post("http://localhost:9000/formSubmit", {
+				fName,
+				lName,
+				phone,
+				gender,
+				year,
+				school,
+				bio,
+			})
+			.then((res) => console.log(res))
+			.catch((err) => console.log(err));
+		/* console.log("button clicked"); */
+	};
 	return props.trigger ? (
-		<section id="form" className="h-screen z-50">
-			<div className="grid grid-cols-12">
-				<div className="col-span-12  bg-[length:100%_100%] formBg h-fit w-fit p-10">
-					<div className="grid 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 mt-32 ml-10 gap-x-16">
+		<section id="form" className="z-50">
+			<div className="grid grid-cols-12  max-h-[calc(100vh-10%)] h-screen overflow-y-scroll">
+				<div className="col-span-12  bg-cover formBg h-fit w-fit p-10">
+					<div className="grid grid-cols-1 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 xs:grid-cols-1 mt-1 2xl:mt-32 xl:mt-32 lg:mt md:mt-1 sm:mt-1 xs:mt-1 2xl:ml-10 xl:ml-10 lg:ml-10 md:ml-10 sm:ml-1 xs:ml-1 2xl:gap-x-16 xl:gap-x-16 lg:gap-x-16 md:gap-x-8 sm:gap-x-2 xs:gap-x-1 gap-x-2">
 						<div className="col-span-1">
 							<div className="label">
 								<label id="fname" className="text-white ">
@@ -17,6 +40,7 @@ export default function Form(props) {
 									type="text"
 									className="w-full outline-none text-white p-2 bg-transparent border-[#d9d9d9] border-2 rounded-xl"
 									name="fname"
+									onChange={(e) => setFName(e.target.value)}
 								/>
 							</div>
 
@@ -30,6 +54,7 @@ export default function Form(props) {
 									type="text"
 									className="w-full outline-none text-white p-2 bg-transparent border-[#d9d9d9] border-2 rounded-xl"
 									name="lname"
+									onChange={(e) => setLName(e.target.value)}
 								/>
 							</div>
 
@@ -43,6 +68,7 @@ export default function Form(props) {
 									type="text"
 									className="w-full outline-none text-white p-2 bg-transparent border-[#d9d9d9] border-2 rounded-xl"
 									name="phone"
+									onChange={(e) => setPhone(e.target.value)}
 								/>
 							</div>
 
@@ -56,6 +82,7 @@ export default function Form(props) {
 									name=""
 									id="gender"
 									className="text-white w-full p-2 bg-transparent border-2 border-[#d9d9d9]outline-none"
+									onChange={(e) => setGender(e.target.value)}
 								>
 									<option value=""></option>
 									<option value="Male">Male</option>
@@ -74,6 +101,7 @@ export default function Form(props) {
 									type="text"
 									className="w-full outline-none text-white p-2 bg-transparent border-[#d9d9d9] border-2 rounded-xl"
 									name="gradYear"
+									onChange={(e) => setYear(e.target.value)}
 								/>
 							</div>
 
@@ -87,6 +115,7 @@ export default function Form(props) {
 									type="text"
 									className="w-full outline-none text-white p-2 bg-transparent border-[#d9d9d9] border-2 rounded-xl"
 									name="school"
+									onChange={(e) => setSchool(e.target.value)}
 								/>
 							</div>
 						</div>
@@ -97,17 +126,19 @@ export default function Form(props) {
 									BIO
 								</label>
 							</div>
-							<div className="inputBio mr-16">
+							<div className="inputBio 2xl:mr-16 xl:mr-16 lg:mr-16 md:mr-8 sm:mr-2 xs:mr-2 mr-0">
 								<textarea
 									name="bio"
 									id=""
 									rows="5"
 									className="bg-transparent text-white border-2 border-[#d9d9d9] rounded-lg w-full p-2"
+									onChange={(e) => setBio(e.target.value)}
 								></textarea>
 							</div>
 						</div>
 					</div>
 					<button
+						onClick={handleClick}
 						className="bg-transparent border-2 border-[#d9d9d9] mt-6 px-3 py-1 rounded-lg ml-10"
 						type="submit"
 					>
